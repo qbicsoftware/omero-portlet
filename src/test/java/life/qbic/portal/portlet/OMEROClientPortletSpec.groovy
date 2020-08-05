5f33c51e1316f4b9ceb60d2be62e33ca61c71462
Tobias Koch <tobias.koch@qbic.uni-tuebingen.de>
2020-08-05 16:38:09 +0200

    Add Spock framework and empty test class

---
+++ src/test/java/life/qbic/portal/portlet/OMEROClientPortletSpec.groovy
@@ -0,0 +1,12 @@
+package life.qbic.portal.portlet
+
+/**
+ * <short description>
+ *
+ * <detailed description>
+ *
+ * @since: 1.0.0
+ * @author: Tobias Koch
+ */
+class OMEROClientPortletSpec extends spock.lang.Specification {
+}
