43324190582de63c9be6f3d053e31d0dd2ccdfbd
Sven Fillinger <sven.fillinger@qbic.uni-tuebingen.de>
2020-10-06 10:48:59 +0200

    Add Luis Kuhn back again as Copyright holder

--- docs/conf.py
+++ docs/conf.py
@@ -47,8 +47,8 @@ master_doc = 'index'
 
 # General information about the project.
 project = 'omero-portlet'
-copyright = "2020, Sven Fillinger"
-author = "Sven Fillinger"
+copyright = "2020, Luis Kuhn"
+author = "Luis Kuhn"
 
 # The version info for the project you're documenting, acts as replacement
 # for |version| and |release|, also used in various other places throughout
