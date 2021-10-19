1a6d63763c26bd58d00cf08a5ae5b39a323e561e
luiskuhn <luis.kuhn@gmail.com>
2021-10-19 17:56:02 +0200

    cleaned imports

--- src/main/java/life/qbic/portal/portlet/OMEROClientPortlet.java
+++ src/main/java/life/qbic/portal/portlet/OMEROClientPortlet.java
@@ -18,7 +18,6 @@ import java.util.Collection;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map.Entry;
-import java.util.concurrent.atomic.AtomicInteger;
 
 import life.qbic.omero.BasicOMEROClient;
 import life.qbic.portal.utils.ConfigurationManager;
